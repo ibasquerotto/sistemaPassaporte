309c912cd55dd9802ba67a1e3b83891167be7b27
Igor Basquerotto <basquerotto@gmail.com>
2026-05-21 02:28:45 -0300

    Spring Initializr

---
+++ src/main/java/br/edu/fateclins/pweb/sistemaPassaporte/SistemaPassaporteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fateclins.pweb.sistemaPassaporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPassaporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPassaporteApplication.class, args);
+	}
+
+}
